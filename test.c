573980597424680cabc07ebfa8ac3a966e874367
victor-fdez <victor.j.fdez@gmail.com>
2013-03-30 19:54:12 -0400

    message

--- test.c
+++ test.c
@@ -3,9 +3,12 @@ int main(int argc, char** argv)
 {
 	int a[10][30];
 	int i, j;
-	for(i = 0; i < 11; i++)
+	for(i = 0; i < 10; i++)
 	{
-		a[i][0] = i;
+		for(j = 0; j < 30; j++)
+		{
+			a[i][j] = a[0][j] + a[0][j+1];
+		}
 	}
 }
 
