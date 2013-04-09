350b31aab5fcbd0a9b02ae460b4dcf06c35ea5e3
chris porter <cporterbox@gmail.com>
2013-04-08 20:25:44 -0400

    fixed loop pass bug

--- loop_hoisting.c
+++ loop_hoisting.c
@@ -5,63 +5,45 @@ int main()
 {
 
         // simple increment example
-        /*int a[100];
-	int i;
+        int a[100];
+        int ad[10][10];
+	int i, j, variant;
+        int invariant = 0;
 
-	for (i = 0; i < 100; ++i) {
+	/*for (i = 0; i < 100; ++i) {
 		a[i] = 10;
 	}*/
 
         // simple decrement example 
-        /*int a[100];
-	int i;
-
-	for (i = 99; i >= 0; --i) {
+	/*for (i = 99; i >= 0; --i) {
                 printf("it'ing\n");
 		a[i] = 10;
 	}*/
 
 
         // trivial invariant example
-        /*int top = 100;
-	int a[top];
-	int i;
-        int variant = 0;
-
-
-	for (i = 0; i < 100; ++i) {
-                int invariant = 0;
+	/*for (i = 0; i < 100; ++i) {
                 variant = invariant;
 		a[invariant] = 10;
 		//a[invariant + 1] = 10; // this case kills me
 
-                //for (int j = 0; j < 10; j++) {
-                //        a[j] = 2;
-                //}
                 if (variant == invariant)
                         break;
-
         }*/
 
         // nested loop with only 1 invariant
-        /*int i;
-        int j;
-        int a[10][10];
-        for(i = 0; i < 10; ++i){
+        /*for(i = 0; i < 10; ++i){
                 for(j = 0; j < 10; ++j){
-                        a[i][1] = 1;
+                        ad[i][1] = 1;
                 }
         }*/
 
-        int i;
-        int j;
-        int a[10][10];
+        // nested loop with two increments
         for(i = 0; i < 10; ++i){
                 for(j = 0; j < 10; ++j){
-                        a[i][j] = 1;
+                        ad[i][j] = 1;
                 }
         }
-
 
 
 
@@ -69,7 +51,6 @@ int main()
         //funky();
 
         printf("yo yo ma\n");
-        //int oo = 0;
 	return 0;
 }
 
